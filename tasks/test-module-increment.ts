c20518cd7167a052f60151ad419a6f945b88c09b
Kimmo <kkpsiren@gmail.com>
2023-06-10 16:47:21 +0200

    increasing collects done! First collect 1 matic, second collect 2 matic... user can set this

--- tasks/test-module-increment.ts
+++ tasks/test-module-increment.ts
@@ -1,11 +1,12 @@
 import { defaultAbiCoder } from 'ethers/lib/utils';
 import { task } from 'hardhat/config';
 import {
-  FollowNFT__factory,
   LensHub__factory,
   IncrementingFeeCollectModule__factory,
+  Currency__factory,
 } from '../typechain-types';
-import { CreateProfileDataStruct } from '../typechain-types/LensHub';
+import { CreateProfileDataStruct, PostDataStruct } from '../typechain-types/LensHub';
+import { BigNumberish } from 'ethers';
 import {
   deployContract,
   getAddrs,
@@ -15,17 +16,31 @@ import {
   ZERO_ADDRESS,
 } from './helpers/utils';
 
-task('test-module', 'tests the Increment Fee Collect').setAction(async ({}, hre) => {
+const counter = 1;
+task('test-module-increment', 'tests the Increment Fee Collect').setAction(async ({}, hre) => {
   const [governance, , user, user2] = await initEnv(hre);
   const addrs = getAddrs();
   const lensHub = LensHub__factory.connect(addrs['lensHub proxy'], governance);
 
   await waitForTx(lensHub.setState(ProtocolState.Unpaused));
   await waitForTx(lensHub.whitelistProfileCreator(user.address, true));
+  await waitForTx(lensHub.whitelistProfileCreator(user2.address, true));
+
+  const currency = Currency__factory.connect(addrs['currency'], governance);
+
+  // give governance some erc20
+  const mintAmount = 100;
+  await waitForTx(currency.mint(user2.address, mintAmount));
+
+  // payer setup
+  await waitForTx(
+    currency.connect(user2).approve(addrs['incrementing fee collect module'], mintAmount)
+  );
+  console.log('erc20 approval DONE');
 
   const inputStruct: CreateProfileDataStruct = {
     to: user.address,
-    handle: 'zer0dot',
+    handle: `zer0dotone12${counter}`,
     imageURI:
       'https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan',
     followModule: ZERO_ADDRESS,
@@ -33,36 +48,60 @@ task('test-module', 'tests the Increment Fee Collect').setAction(async ({}, hre)
     followNFTURI:
       'https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan',
   };
-  await waitForTx(lensHub.connect(user).createProfile(inputStruct));
-  await waitForTx(lensHub.connect(user2).createProfile(inputStruct));
 
-  const incrementingFee = LensHub__factory.connect(
+  const inputStruct2: CreateProfileDataStruct = {
+    to: user.address,
+    handle: `zer0dottwo12${counter}`,
+    imageURI:
+      'https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan',
+    followModule: ZERO_ADDRESS,
+    followModuleInitData: [],
+    followNFTURI:
+      'https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan',
+  };
+
+  await waitForTx(lensHub.connect(user).createProfile(inputStruct));
+  await waitForTx(lensHub.connect(user2).createProfile(inputStruct2));
+
+  const incrementingFeeCollectModule = IncrementingFeeCollectModule__factory.connect(
     addrs['incrementing fee collect module'],
     governance
   );
 
-  await waitForTx(lensHub.whitelistFollowModule(incrementingFee.address, true));
+  await waitForTx(lensHub.whitelistCollectModule(addrs['incrementing fee collect module'], true));
 
-  //await waitForTx(lensHub.connect(user).setFollowModule(1, secretCodeFollowModule.address, data));
-  //const badData = defaultAbiCoder.encode(['uint256'], ['1337']);
-  //const data = defaultAbiCoder.encode(['uint256'], ['42069']);
-  //
-  //await waitForTx(lensHub.connect(user).follow([1], [badData]));
-  //  //try {
-  //  //} catch (e) {
-  //
-  //  //  console.log(`Expected failure occurred! Error: ${e}`);
-  //  //}
-  //  //await waitForTx(lensHub.connect(user).follow([1], [data]));
-  //
-  //  //const followNFTAddr = await lensHub.getFollowNFT(1);
-  //  //const followNFT = FollowNFT__factory.connect(followNFTAddr, user);
-  //
-  //  //const totalSupply = await followNFT.totalSupply();
-  //  //const ownerOf = await followNFT.ownerOf(1);
-  //
-  //  console.log(`Follow NFT total supply (should be 1): ${totalSupply}`);
-  //  console.log(
-  //    `Follow NFT owner of ID 1: ${ownerOf}, user address (should be the same): ${user.address}`
-  //  );
+  const amount = 10;
+  const increasingAmount = 1;
+  const followerOnly = false; // need to follow them maybe to collect?
+  const referralFee = 1;
+  const currencyAddress = addrs['currency'];
+
+  const data = defaultAbiCoder.encode(
+    ['uint256', 'uint256', 'address', 'address', 'uint16', 'bool'],
+    [amount, increasingAmount, currencyAddress, user.address, referralFee, followerOnly]
+  );
+  const postData: PostDataStruct = {
+    profileId: 1,
+    contentURI: 'https://ipfs.io/ipfs/Qmby8QocUU2sPZL46rZeMctAuF5nrCc7eR1PPkooCztWPz',
+    collectModule: addrs['incrementing fee collect module'],
+    collectModuleInitData: data,
+    referenceModule: ZERO_ADDRESS,
+    referenceModuleInitData: [],
+  };
+
+  await waitForTx(lensHub.connect(user).post(postData));
+  console.log('posted');
+
+  const profileId = 0x01;
+  const pubId = 0x01;
+  const collectData1 = defaultAbiCoder.encode(['address', 'uint256'], [addrs['currency'], amount]);
+
+  await waitForTx(lensHub.connect(user2).collect(profileId, pubId, collectData1));
+  console.log('first collect done');
+  const collectData2 = defaultAbiCoder.encode(
+    ['address', 'uint256'],
+    [addrs['currency'], amount + increasingAmount]
+  );
+  await waitForTx(lensHub.connect(user2).collect(profileId, pubId, collectData2));
+  console.log('second collect done');
 });
